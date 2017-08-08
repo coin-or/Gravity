15cfcf68773047a9c76d2886792e8c54adff596a
tcsnwgl <guanglei.wang13@gmail.com>
2017-08-08 11:36:41 +1000

    new set of results

--- data_sets/Minkcut/generator.sh
+++ data_sets/Minkcut/generator.sh
@@ -1,4 +1,4 @@
 #bin/bash
 
-for Nodes in {10..200};
-do ./rudy -rnd_graph ${Nodes} 50 0 > random_${Nodes}.txt; done
+for Nodes in {10..200..10};
+do ./rudy -rnd_graph ${Nodes} 10 0 > random10_${Nodes}.txt; done
