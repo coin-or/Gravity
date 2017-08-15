4daca8bc00f6e7f7da64f7b615e426dac13e2460
tcsnwgl <guanglei.wang13@gmail.com>
2017-08-15 13:08:31 +1000

    update SDP

--- data_sets/Minkcut/generator.sh
+++ data_sets/Minkcut/generator.sh
@@ -1,8 +1,8 @@
 #/usr/local/bin/bash
-for Nodes in {300..500..100};
+for Nodes in {50..250..50};
 #do ./rudy -rnd_graph ${Nodes} 10 0 > random10_${Nodes}.txt; done
 #do ./rudy -spinglass2pm ${Nodes} ${Nodes} 50 0 > spinglass2pm_${Nodes}${Nodes}.txt; done
 do 
     echo "size ${Nodes}";
-    ./band ${Nodes};
+    sudo ./band ${Nodes};
 done
