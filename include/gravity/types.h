1fe89e514bffda9ede596cf07322476ca243acfe
Hassan <hassan.hijazi@yahoo.com>
2017-09-19 18:30:34 -0600

    Including types in Namespace

--- include/gravity/types.h
+++ include/gravity/types.h
@@ -10,54 +10,55 @@
 
 #include <list>
 #include <string>
-
-typedef unsigned int ind; /* Index type */
-//typedef std::set<ind> indx; /* Set of indices type */
-
-
-typedef enum { linear_, convex_, concave_, undet_} Convexity; /* Convexity Type */
-typedef enum { neg_ = -2, non_pos_ = -1, zero_ = 0, non_neg_ = 1, pos_ = 2, unknown_ = 3} Sign; /* Sign Type */
-typedef enum { binary_, short_, integer_, float_, double_, long_} NType; /* Number Type */
-typedef enum { binary_c, short_c, integer_c, float_c, double_c, long_c, par_c, uexp_c, bexp_c, var_c, func_c, sdpvar_c} CType; /* Constant type, ancestor to parameter, var and function */
-typedef enum { infeasible, optimal, suboptimal, unbounded, error} Outcome;
-typedef enum { geq, leq, eq } ConstraintType;
-typedef enum { const_, lin_, quad_, pol_, nlin_ } FType;  /* Function type in constraint: Constant, Linear, Quadratic, Polynomial or Nonlinear function */
-typedef enum { minimize, maximize } ObjectiveType;
-typedef enum { id_, number_, plus_, minus_, product_, div_, power_, cos_, sin_, sqrt_, exp_, log_} OperatorType;  /* Operation type in the expression tree */
-
-typedef enum { ordered_pairs_, unordered_ } IndexType;  /* Solver type */
-
-using namespace std;
+namespace gravity{
+    typedef unsigned int ind; /* Index type */
+    //typedef std::set<ind> indx; /* Set of indices type */
 
 
-/** Class for manipulating indices */
-class ordered_pairs{
-    
-public:
-    unsigned          _first;
-    unsigned          _last;
-    std::list<string> _keys;
-    std::list<string> _from;
-    std::list<string> _to;
-    ordered_pairs(unsigned p1 ,unsigned p2){
-        _first = p1;
-        _last = p2;
-        string key;
-    for (int i = p1-1; i < p2; i++){
-        for (int j = i+1; j < p2; j++){
-            _keys.push_back(to_string(i) + "," + to_string(j));
-            _from.push_back(to_string(i));
-            _to.push_back(to_string(j));
+    typedef enum { linear_, convex_, concave_, undet_} Convexity; /* Convexity Type */
+    typedef enum { neg_ = -2, non_pos_ = -1, zero_ = 0, non_neg_ = 1, pos_ = 2, unknown_ = 3} Sign; /* Sign Type */
+    typedef enum { binary_, short_, integer_, float_, double_, long_} NType; /* Number Type */
+    typedef enum { binary_c, short_c, integer_c, float_c, double_c, long_c, par_c, uexp_c, bexp_c, var_c, func_c, sdpvar_c} CType; /* Constant type, ancestor to parameter, var and function */
+    typedef enum { infeasible, optimal, suboptimal, unbounded, error} Outcome;
+    typedef enum { geq, leq, eq } ConstraintType;
+    typedef enum { const_, lin_, quad_, pol_, nlin_ } FType;  /* Function type in constraint: Constant, Linear, Quadratic, Polynomial or Nonlinear function */
+    typedef enum { minimize, maximize } ObjectiveType;
+    typedef enum { id_, number_, plus_, minus_, product_, div_, power_, cos_, sin_, sqrt_, exp_, log_} OperatorType;  /* Operation type in the expression tree */
+
+    typedef enum { ordered_pairs_, unordered_ } IndexType;  /* Solver type */
+
+    using namespace std;
+
+
+    /** Class for manipulating indices */
+    class ordered_pairs{
+        
+    public:
+        unsigned          _first;
+        unsigned          _last;
+        std::list<string> _keys;
+        std::list<string> _from;
+        std::list<string> _to;
+        ordered_pairs(unsigned p1 ,unsigned p2){
+            _first = p1;
+            _last = p2;
+            string key;
+        for (int i = p1-1; i < p2; i++){
+            for (int j = i+1; j < p2; j++){
+                _keys.push_back(to_string(i) + "," + to_string(j));
+                _from.push_back(to_string(i));
+                _to.push_back(to_string(j));
+            }
         }
-    }
 
-    }
-};
+        }
+    };
 
-typedef enum { ipopt, gurobi, bonmin, cplex, sdpa_, mosek_} SolverType;  /* Solver type */
+    typedef enum { ipopt, gurobi, bonmin, cplex, sdpa_, mosek_} SolverType;  /* Solver type */
 
-// settings of solvers. used by solvers like sdpa.
-typedef enum {unsolved = -1, penalty=0, fast=1, medium=2, stable=3} SolverSettings;
+    // settings of solvers. used by solvers like sdpa.
+    typedef enum {unsolved = -1, penalty=0, fast=1, medium=2, stable=3} SolverSettings;
 
+}
 
 #endif
