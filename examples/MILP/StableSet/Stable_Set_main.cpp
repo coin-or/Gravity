3e37b10148a3cf52f5cdcb3b007d714268964e47
tcsnwgl <guanglei.wang13@gmail.com>
2017-09-27 10:28:07 +1000

    abstraction

--- examples/MILP/StableSet/Stable_Set_main.cpp
+++ examples/MILP/StableSet/Stable_Set_main.cpp
@@ -226,13 +226,14 @@ int main (int argc, const char * argv[])
 //            }
 //        }
     
-    /* Objective declaration */
-//    constant<int> twos(2);
+/* Objective declaration */
+
+//  constant<int> twos(2);
     auto obj_SDP = 2*sum(Xij) + sum(Xii);
     SDP.set_objective(max(obj_SDP));
     
     solver s1(SDP,ipopt);
-//    solver s1(SDP,cplex);
+//  solver s1(SDP,cplex);
 
     wall0 = get_wall_time();
     cpu0  = get_cpu_time();
