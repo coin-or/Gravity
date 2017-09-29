30b638e62282f9728823d353e4831efe6901980e
tcsnwgl <guanglei.wang13@gmail.com>
2017-09-29 14:09:38 +1000

    SOCOPF base version is ok

--- examples/MINLP/Power/SOCOPF/SOCOPF_main.cpp
+++ examples/MINLP/Power/SOCOPF/SOCOPF_main.cpp
@@ -100,21 +100,23 @@ int main (int argc, const char * argv[])
     // Lifted variables.
     var<double>  R_Wij("R_Wij"); // real part of Wij
     var<double>  Im_Wij("Im_Wij"); // imaginary part of Wij.
-    var<double>  Wii("Wii");
+    var<double>  Wii("Wii", 0, 1);
     SOCP.add_var(Wii^nb_buses);
     SOCP.add_var(R_Wij^nb_lines);
     SOCP.add_var(Im_Wij^nb_lines);
-    Wii.initialize_all(1);
-    
+//    SOCP.add_var(R_Wij^(nb_buses*(nb_buses-1)/2));
+//    SOCP.add_var(Im_Wij^(nb_buses*(nb_buses-1)/2));
     /** Construct the objective function*/
-    func_ obj = sum(grid->c0) + sum(grid->c1,Pg) + sum(grid->c2, power(Pg, 2));
+    func_ obj = sum(grid->c0) + sum(grid->c1, Pg) + sum(grid->c2, power(Pg, 2));
+
     SOCP.set_objective(min(obj));
     
     /** Define constraints */
     /* SOCP constraints */
     Constraint SOC("SOC");
-    ordered_pairs indices(1, nb_buses);
-    SOC = power(R_Wij.in(grid->arcs), 2) + power(Im_Wij.in(grid->arcs), 2) - Wii.from(grid->arcs)*Wii.to(grid->arcs);
+//    ordered_pairs indices(1, nb_buses);
+//    SOC =  power(R_Wij.in(indices), 2) - Wii.from(indices)*Wii.to(indices) ;
+    SOC =  power(R_Wij.in(grid->arcs), 2) - Wii.from(grid->arcs)*Wii.to(grid->arcs) ;
     SOCP.add_constraint(SOC <= 0);
     
     //KCL
@@ -122,19 +124,19 @@ int main (int argc, const char * argv[])
         Bus* bus = (Bus*) b;
         Constraint KCL_P("KCL_P"+bus->_name);
         Constraint KCL_Q("KCL_Q"+bus->_name);
-        
+
         /* Power Conservation */
         KCL_P  = sum(Pf_from.in(b->get_out())) + sum(Pf_to.in(b->get_in())) + bus->pl()- sum(Pg.in(bus->_gen));
         KCL_Q  = sum(Qf_from.in(b->get_out())) + sum(Qf_to.in(b->get_in())) + bus->ql()- sum(Qg.in(bus->_gen));
-        
+
         /* Shunts */
         KCL_P +=  bus->gs()*Wii(bus->ID+1);
         KCL_Q -=  bus->bs()*Wii(bus->ID+1);
-        
+
         SOCP.add_constraint(KCL_P = 0);
         SOCP.add_constraint(KCL_Q = 0);
     }
-    
+
     //AC Power Flow.
     //
     Constraint Flow_P_From("Flow_P_From");
@@ -168,7 +170,7 @@ int main (int argc, const char * argv[])
     Flow_Q_To += grid->g_tf.in(grid->arcs)*Im_Wij.in(grid->arcs);
     Flow_Q_To = 0;
     SOCP.add_constraint(Flow_Q_To);
-    
+
     // AC voltage limit constraints.
     Constraint Vol_limit_UB("Vol_limit_UB");
     Vol_limit_UB = Wii.in(grid->nodes);
@@ -179,7 +181,7 @@ int main (int argc, const char * argv[])
     Vol_limit_LB = Wii.in(grid->nodes);
     Vol_limit_LB -= power(grid->v_min.in(grid->nodes),2);
     SOCP.add_constraint(Vol_limit_LB >= 0);
-    
+
     /* Phase Angle Bounds constraints */
     Constraint PAD_UB("PAD_UB");
     PAD_UB = Im_Wij.in(grid->arcs);
@@ -201,9 +203,9 @@ int main (int argc, const char * argv[])
     Thermal_Limit_to += power(Pf_to.in(grid->arcs), 2) + power(Qf_to.in(grid->arcs), 2);
     Thermal_Limit_to -= power(grid->S_max.in(grid->arcs),2);
     SOCP.add_constraint(Thermal_Limit_to <= 0);
-//
-    //solver SCOPF(SOCP,cplex);
-   solver SCOPF(SOCP,ipopt);
+////
+   solver SCOPF(SOCP,cplex);
+   //solver SCOPF(SOCP,ipopt);
 
     SCOPF.run();
     return 0;
