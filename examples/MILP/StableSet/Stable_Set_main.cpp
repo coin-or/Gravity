79b0c3b8c709980953e3dd15092ca73b6f6643d1
Hassan <hassan.hijazi@yahoo.com>
2017-09-30 20:41:03 -0600

    Cleaning up main files

--- examples/MILP/StableSet/Stable_Set_main.cpp
+++ examples/MILP/StableSet/Stable_Set_main.cpp
@@ -19,53 +19,7 @@
 
 using namespace std;
 using namespace gravity;
-#define EPS 0.000001
-#define DebugOn(x) cout << x
-#define DebugOff(x)
-//  Windows
-#ifdef _WIN32
-#include <Windows.h>
-double get_wall_time(){
-    LARGE_INTEGER time,freq;
-    if (!QueryPerformanceFrequency(&freq)){
-        //  Handle error
-        return 0;
-    }
-    if (!QueryPerformanceCounter(&time)){
-        //  Handle error
-        return 0;
-    }
-    return (double)time.QuadPart / freq.QuadPart;
-}
-double get_cpu_time(){
-    FILETIME a,b,c,d;
-    if (GetProcessTimes(GetCurrentProcess(),&a,&b,&c,&d) != 0){
-        //  Returns total user time.
-        //  Can be tweaked to include kernel times as well.
-        return
-        (double)(d.dwLowDateTime |
-                 ((unsigned long long)d.dwHighDateTime << 32)) * 0.0000001;
-    }else{
-        //  Handle error
-        return 0;
-    }
-}
-//  Posix/Linux
-#else
-#include <time.h>
-#include <sys/time.h>
-double get_wall_time(){
-    struct timeval time;
-    if (gettimeofday(&time,NULL)){
-        //  Handle error
-        return 0;
-    }
-    return (double)time.tv_sec + (double)time.tv_usec * .000001;
-}
-double get_cpu_time(){
-    return (double)clock() / CLOCKS_PER_SEC;
-}
-#endif
+
 
 int main (int argc, const char * argv[])
 {
