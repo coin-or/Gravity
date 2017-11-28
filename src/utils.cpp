85878cb2ce5060ad78ec180fa7b09da44840b0d6
Hassan <hassan.hijazi@anu.edu.au>
2017-11-28 11:24:37 -0700

    parallel ising implemented

--- src/utils.cpp
+++ src/utils.cpp
@@ -35,3 +35,24 @@ double get_cpu_time() {
     return (double)clock() / CLOCKS_PER_SEC;
 }
 #endif
+
+
+//Split "mem" into "parts", e.g. if mem = 10 and parts = 4 you will have: 0,2,4,6,10, i.e., [0,2], [2,4], [4,6], [6,10]
+//if possible the function will split mem into equal chuncks, if not
+//the last chunck will be slightly larger
+
+std::vector<int> bounds(int parts, int mem) {
+    std::vector<int>bnd;
+    int delta = mem / parts;
+    int reminder = mem % parts;
+    int N1 = 0, N2 = 0;
+    bnd.push_back(N1);
+    for (int i = 0; i < parts; ++i) {
+        N2 = N1 + delta;
+        if (i == parts - 1)
+            N2 += reminder;
+        bnd.push_back(N2);
+        N1 = N2;
+    }
+    return bnd;
+}
