2f5ca3a15c0532e8c85b50c46db305bbcd9c2a7c
Hassan <hassan.hijazi@yahoo.com>
2018-01-24 15:30:38 -0700

    multithrad bug and memory leaks fixed

--- examples/MINLP/Power/ACOPF/ACOPF_main.cpp
+++ examples/MINLP/Power/ACOPF/ACOPF_main.cpp
@@ -21,7 +21,7 @@ using namespace gravity;
 
 int main (int argc, char * argv[])
 {
-    string fname = "../data_sets/Power/nesta_case5_pjm.m", mtype = "ACRECT";    
+    string fname = "../data_sets/Power/nesta_case5_pjm.m", mtype = "ACPOL";
     DebugOn("argv[0] =" << argv[0] << endl);
     string path = argv[0];
     int output = 0;
@@ -32,9 +32,9 @@ int main (int argc, char * argv[])
     /** create a OptionParser with options */
     op::OptionParser opt;
     opt.add_option("h", "help", "shows option help"); // no default value means boolean options, which default value is false
-    opt.add_option("f", "file", "Input file name", fname );
+    opt.add_option("f", "file", "Input file name (def. ../data_sets/Power/nesta_case5_pjm.m)", fname );
     opt.add_option("l", "log", "Log level (def. 0)", log_level );
-    opt.add_option("m", "model", "power flow model: ACPOL/ACRECT", mtype );
+    opt.add_option("m", "model", "power flow model: ACPOL/ACRECT (def. ACPOL)", mtype );
     
     /** parse the options and verify that all went well. If not, errors and help will be shown */
     bool correct_parsing = opt.parse_options(argc, argv);
