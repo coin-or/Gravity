fce38279d5a958c91d2443b0a0d1e24539987889
guanglei1wang <guanglei.wang13@gmail.com>
2018-02-20 09:47:41 +1100

    remove the constraint print bug in acuc

--- examples/MINLP/Power/ACUC/ACUC.cpp
+++ examples/MINLP/Power/ACUC/ACUC.cpp
@@ -124,7 +124,7 @@ int main (int argc, const char * argv[])
     Constraint KCL_P("KCL_P");
     Constraint KCL_Q("KCL_Q");
     KCL_P  = sum(Pf_from.out_arcs()) + sum(Pf_to.in_arcs())- sum(Pg.in_gens()) + grid->gs*Wii+ grid->pl;
-    //KCL_P.print(true);
+    KCL_P.print();
     ACUC.add_constraint(KCL_P.in(grid->nodes, T) == 0);
 
 //    KCL_Q  = sum(Qf_from.out_arcs()) + sum(Qf_to.in_arcs()) + grid->ql - sum(Qg.in_gens()) - grid->bs*Wii;
