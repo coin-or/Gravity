26b6574783f5d74b76e342a09c6a1f963f7b59cb
Hassan <hlh@lanl.gov>
2018-11-20 19:52:10 -0700

    adding cmake dir

---
+++ cmake/FindBONMIN.cmake
@@ -0,0 +1,45 @@
+set(BONMIN_ROOT_DIR "$ENV{BONMIN_ROOT_DIR}" CACHE PATH "BONMIN root directory.")
+
+message("Looking for Bonmin in ${BONMIN_ROOT_DIR}")
+
+#string(REGEX MATCH "[0-9]+" BONMIN_VERSION "${BONMIN_ROOT_DIR}")
+
+#message("Ipopt version ${BONMIN_VERSION}")
+
+#string(SUBSTRING ${BONMIN_VERSION} 0 2 BONMIN_VERSION_SHORT)
+
+
+find_path(BONMIN_INCLUDE_DIR
+        NAMES BonTMINLP.hpp
+        HINTS /usr/local/include/coin
+        HINTS ${BONMIN_ROOT_DIR}/include/coin
+        HINTS ${BONMIN_ROOT_DIR}/include
+)
+
+if(APPLE)
+    find_library(BONMIN_LIBRARY
+            libbonmin.dylib
+            HINTS /usr/local/lib
+            HINTS ${BONMIN_ROOT_DIR}/lib
+    )
+elseif(UNIX)
+    find_library(BONMIN_LIBRARY
+            libbonmin.so
+            HINTS /usr/local/lib
+            HINTS ${BONMIN_ROOT_DIR}/lib
+    )
+endif()
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(BONMIN DEFAULT_MSG BONMIN_LIBRARY BONMIN_INCLUDE_DIR)
+
+if(BONMIN_FOUND)
+    message("—- Found Bonmin under ${BONMIN_INCLUDE_DIR}")
+    set(BONMIN_INCLUDE_DIRS ${BONMIN_INCLUDE_DIR})
+    set(BONMIN_LIBRARIES ${BONMIN_LIBRARY})
+    if(CMAKE_SYSTEM_NAME STREQUAL "Linux")
+        set(BONMIN_LIBRARIES "${BONMIN_LIBRARIES};m;pthread")
+    endif(CMAKE_SYSTEM_NAME STREQUAL "Linux")
+endif(BONMIN_FOUND)
+
+mark_as_advanced(BONMIN_LIBRARY BONMIN_INCLUDE_DIR)
