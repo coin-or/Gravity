9b8524c1e121557b1133bd4d7176deba8c72e67a
Hassan <hlh@lanl.gov>
2018-12-09 11:52:24 -0500

    Update SDPCUTS_main.cpp

--- examples/Optimization/NonLinear/Power/SDPCUTS/SDPCUTS_main.cpp
+++ examples/Optimization/NonLinear/Power/SDPCUTS/SDPCUTS_main.cpp
@@ -61,7 +61,7 @@ int main (int argc, char * argv[]) {
     else if(solver_str.compare("cplex")==0) {
         solv_type = cplex;
     }else if(solver_str.compare("Mosek")==0) {
-        solv_type = Mosek;
+        solv_type = mosek;
     }
     lazy = opt["lz"];
     if (lazy.compare("no")==0) {
