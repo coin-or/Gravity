83ba904ce74c73f387b3c021f43301f27c262554
Hassan <hassan.hijazi@anu.edu.au>
2019-07-15 22:05:52 -0600

    fixing obbt with MPI

--- include/gravity/solver.h
+++ include/gravity/solver.h
@@ -603,7 +603,23 @@ namespace gravity {
                         }
                     }
                 }
-                else {                    
+                else {
+                    DebugOn("I'm worker ID: " << worker_id << ", I will be sending my solutions to all workers " << endl);
+                    for (auto i = limits[worker_id]; i < limits[worker_id+1]; i++) {
+                        auto model = models[i];
+                        auto nb_vars = model->get_nb_vars();
+                        vector<double> solution;
+                        solution.resize(nb_vars);
+                        model->get_solution(solution);
+                        for (auto w_id = 0; w_id<nb_workers_; w_id++) {
+                            if (worker_id == w_id){
+                                continue;
+                            }
+                            DebugOn("I'm worker ID: " << worker_id << ", I finished loading solution of task " << i << endl);
+                            MPI_Isend(&solution[0], nb_vars, MPI_DOUBLE, w_id, i, MPI_COMM_WORLD,nullptr);
+                            DebugOn("I'm worker ID: " << worker_id << ", I finished sending solution of task " << i << "to worker " << w_id << endl);
+                        }
+                    }
                     DebugOn("I'm worker ID: " << worker_id <<", I'm waiting for the solutions broadcasted by the other workers " << endl);
                     for (auto w_id = 0; w_id<nb_workers_; w_id++) {
                         if (worker_id == w_id){
@@ -618,22 +634,6 @@ namespace gravity {
                             MPI_Recv(&solution[0], nb_vars, MPI_DOUBLE, w_id, i, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
                             DebugOn("I'm worker ID: " << worker_id <<", I received the solution of task " << i << " broadcasted by worker " << w_id << endl);
                             model->set_solution(solution);
-                        }
-                    }
-                    DebugOn("I'm worker ID: " << worker_id << ", I will be sending my solutions to all workers " << endl);
-                    for (auto i = limits[worker_id]; i < limits[worker_id+1]; i++) {
-                        auto model = models[i];
-                        auto nb_vars = model->get_nb_vars();
-                        vector<double> solution;
-                        solution.resize(nb_vars);
-                        model->get_solution(solution);
-                        for (auto w_id = 0; w_id<nb_workers_; w_id++) {
-                            if (worker_id == w_id){
-                                continue;
-                            }
-                            DebugOn("I'm worker ID: " << worker_id << ", I finished loading solution of task " << i << endl);
-                            MPI_Send(&solution[0], nb_vars, MPI_DOUBLE, w_id, i, MPI_COMM_WORLD);
-                            DebugOn("I'm worker ID: " << worker_id << ", I finished sending solution of task " << i << "to worker " << w_id << endl);
                         }
                     }
                 }
