eeb54a7f4cabfade3a4352bd6bad19037ef93801
Tillmann Weisser <tweisser@pn1931910.lanl.gov>
2019-12-09 17:03:19 -0700

    add examples

--- examples/Lasserre/lasserre.cpp
+++ examples/Lasserre/lasserre.cpp
@@ -32,15 +32,16 @@ function lasserre_relax( pop, degree )
     sdpmodel = SDP model
     var_dict = empty dictionary linking monomials to new variables
     for bag in bags
-        add new variables to sdpmodel and keep track in var_ditc
+        add new variables to sdpmodel and keep track in var_ditct
         /**
-         mvec = monomials in variables of bag up to degree
+         mvec = monomials_with_conjugate(bag, degree)
          for m in mvec
             if !(m in keys(var_dict))
              var_dict(m) = newvar(sdpmodel)
             end
          end
          */
+        mvec = monomials(bag, degree)
         add psd constaint lift_matrix(outer_product(mvec), var_dict)
         
         for ieq in ind_eq
@@ -57,12 +58,45 @@ function lasserre_relax( pop, degree )
     end
     add objective.sense objective lift(objective.funciton, var_dict)
     return spdmodel
-    end
+end
+
+
+
+function monomials(variables, degree)
+    /**
+     return all monomials in variables up to degree
+     example: variables = [ x y ] degree = 2
+     return: [1 x y x^2 xy y^2]
+     */
+end
+
+function monomials_with_conjugate(variables, degree)
+/**
+ return all monomials in variables  and their conjugate. If the variables are declared to be real monomials(variables, degree) should be returned instead.
+ example: variables = [ x y ] degree = 2
+ return: [1 x x* y y* x^2 xx* xy xy* x*^2 x*y x*y* y^2 yy* y*^2 ]
+ */
+
+end
 
 function outer_product(mvec)
     /**
      return outer product of mvec with itself.
-     */
+     example mvec = [1 x y]
+     return         [ 1     x        y
+              x*    x*x     x*y
+              y*    xy*     y*y ]
+          
+    example mvec = [1 x y x^2 xy y^2]
+    return  [   1            x            y            x^2          xy          y^2
+           x*        x*x         x*y         x*x^2       x*xy       x*y^2
+           y*        y*x         y*y         y*x^2       y*xy       y*y^2
+          x*^2   x*^2x     x*^2y       (x*x)^2   x*^2xy     (x*y)^2
+          (xy)*  (xy)*x     (xy)*y     (xy)*x^2  (xy)*(xy)  (xy)*y^2
+          y*^2   y*^2x     y*^2y      y*^2x^2  y*^2*xy    y*^2y^2 ]
+        
+     ####   in case the variables are real x*=x
+      */
 end
 
 function multiply(pterm, monomial)
@@ -78,12 +112,14 @@ funciton multiply(polynomial, monomial)
 end
 
 function multiply_matrix(polynomial,  monomial_matix)
-
+/**
+ return matrix of size monomial_matrix where the entry i is the result of multiply( polynomial, monomial_matrix(i) )
+ */
 end
 
 function lift(polynomial, vardict)
     /**
-        replaces every monomial  polynomial by its corresponding entry in vardict
+        replaces every monomial  in polynomial by its corresponding entry in vardict
      */
 end
        
