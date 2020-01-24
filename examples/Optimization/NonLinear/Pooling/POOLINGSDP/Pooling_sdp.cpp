7ea8184fc3f452e8a4786783b4e7d76ff39fc2be
Hassan <hlh@lanl.gov>
2020-01-23 20:21:21 -0700

    fixing nb_threads in pooling_sdp

--- examples/Optimization/NonLinear/Pooling/POOLINGSDP/Pooling_sdp.cpp
+++ examples/Optimization/NonLinear/Pooling/POOLINGSDP/Pooling_sdp.cpp
@@ -364,7 +364,7 @@ int main (int argc, char * argv[]) {
     unsigned max_iter=1e3, nb_threads = 10;
     DebugOn("nb_threads= "<<nb_threads);
     SolverType ub_solver_type = ipopt, lb_solver_type = ipopt;
-    auto status = SPP_NC->run_obbt(SPP, max_time, max_iter, nb_threads=4, ub_solver_type, lb_solver_type, ub_solver_tol, lb_solver_tol, range_tol);
+    auto status = SPP_NC->run_obbt(SPP, max_time, max_iter, nb_threads=1, ub_solver_type, lb_solver_type, ub_solver_tol, lb_solver_tol, range_tol);
 //    SPP->print();
 //    SPP->print_solution();
 //    SPP->print_constraints_stats(1e-6);
