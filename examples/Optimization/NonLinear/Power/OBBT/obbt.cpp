6c4660531b784ef3cd52dcce7664bbc7eb53542e
Hassan <hassan.hijazi@anu.edu.au>
2020-01-28 11:20:36 -0700

    fixing travis

--- examples/Optimization/NonLinear/Power/OBBT/obbt.cpp
+++ examples/Optimization/NonLinear/Power/OBBT/obbt.cpp
@@ -185,7 +185,7 @@ int main (int argc, char * argv[]) {
     if(!linearize){
         auto nonlin_obj=true;
         auto SDP= build_SDPOPF(grid, current, nonlin_obj, sdp_kim);
-        auto res=OPF->run_obbt(SDP,max_time,max_iter,nb_threads,ub_solver_type,lb_solver_type, ub_solver_tol, lb_solver_tol, range_tol);
+        auto res=OPF->run_obbt(SDP,max_time,max_iter,nb_threads=1,ub_solver_type,lb_solver_type, ub_solver_tol, lb_solver_tol, range_tol);
         lower_bound = SDP->get_obj_val();
         lower_bound_init = get<3>(res);
         total_iter=get<1>(res);
@@ -196,7 +196,7 @@ int main (int argc, char * argv[]) {
         current=false;
         auto nonlin_obj=false;
         auto SDP= build_SDPOPF(grid, current, nonlin_obj, sdp_kim);
-        auto res=OPF->run_obbt(SDP,max_time,max_iter,nb_threads=10,ub_solver_type,lb_solver_type, ub_solver_tol, lb_solver_tol, range_tol, true);
+        auto res=OPF->run_obbt(SDP,max_time,max_iter,nb_threads=1,ub_solver_type,lb_solver_type, ub_solver_tol, lb_solver_tol, range_tol, true);
         lower_bound = SDP->get_obj_val();
         lower_bound_init = get<3>(res);
         total_iter=get<1>(res);
