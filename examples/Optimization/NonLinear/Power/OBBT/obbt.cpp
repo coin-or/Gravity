6f2691186e6080b62f50cb32da5c003d6e782972
smitha <smitha@lanl.gov>
2020-03-17 13:18:57 -0600

    gurobi parameter settings

--- examples/Optimization/NonLinear/Power/OBBT/obbt.cpp
+++ examples/Optimization/NonLinear/Power/OBBT/obbt.cpp
@@ -193,7 +193,6 @@ int main (int argc, char * argv[]) {
     double ub_solver_tol=1e-6, lb_solver_tol=1e-8, range_tol=1e-3, opt_rel_tol=1e-2, opt_abs_tol=1e6;
     unsigned max_iter=1e3;
     int oacuts=0, oacuts_init=0;
-    solv_type=gurobi;
     SolverType ub_solver_type = ipopt, lb_solver_type = solv_type;
     linearize=true;
     if(!linearize){
