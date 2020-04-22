3bd20a58b89b118cf2af28ed502ca4b1a4b5d81f
Hassan <hlh@lanl.gov>
2020-04-21 23:16:49 -0600

    add findLASlib.cmake

---
+++ cmake/FindLASlib.cmake
@@ -0,0 +1,29 @@
+set(LASlib_ROOT_DIR "$ENV{LASlib_ROOT_DIR}" CACHE PATH "LASlib root directory.")
+message("Looking for Ipopt in ${LASlib_ROOT_DIR}")
+
+
+find_path(LASlib_INCLUDE_DIR
+	NAMES lasutility.hpp
+	HINTS /usr/local/include/LASlib
+	HINTS ${LASlib_ROOT_DIR}/include
+)
+
+find_library(LASlib_LIBRARY 
+	libLASlib.a
+	HINTS /usr/local/lib/LASlib
+	HINTS ${LASlib_ROOT_DIR}/lib
+)
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(LASlib DEFAULT_MSG LASlib_LIBRARY LASlib_INCLUDE_DIR)
+
+if(LASlib_FOUND)
+	message("—- Found LASlib under ${LASlib_INCLUDE_DIR}")
+    set(LASlib_INCLUDE_DIRS ${LASlib_INCLUDE_DIR})
+    set(LASlib_LIBRARIES ${LASlib_LIBRARY})
+    if(CMAKE_SYSTEM_NAME STREQUAL "Linux")
+        set(LASlib_LIBRARIES "${LASlib_LIBRARIES};m;pthread")
+    endif(CMAKE_SYSTEM_NAME STREQUAL "Linux")
+endif(LASlib_FOUND)
+
+mark_as_advanced(LASlib_LIBRARY LASlib_INCLUDE_DIR)
