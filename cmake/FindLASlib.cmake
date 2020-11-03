82c2685f2686d9ace82fd98d65f694797b15c783
Hassan <hlh@lanl.gov>
2020-11-03 12:41:41 -0700

    travis final

--- cmake/FindLASlib.cmake
+++ cmake/FindLASlib.cmake
@@ -10,6 +10,14 @@ find_path(LASlib_INCLUDE_DIR
 	HINTS ${LASlib_ROOT_DIR}
 )
 
+find_path(LASlib_INCLUDE_DIR2
+        NAMES mydefs.hpp
+        HINTS /usr/local/include/LASzip
+        HINTS ${LASlib_ROOT_DIR}../LASzip/src
+        HINTS ${LASlib_ROOT_DIR}/include
+        HINTS ${LASlib_ROOT_DIR}/inc
+)
+
 find_library(LASlib_LIBRARY 
 	liblas.a
 	HINTS /usr/local/lib/LASlib
@@ -18,11 +26,11 @@ find_library(LASlib_LIBRARY
 )
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(LASlib DEFAULT_MSG LASlib_LIBRARY LASlib_INCLUDE_DIR)
+find_package_handle_standard_args(LASlib DEFAULT_MSG LASlib_LIBRARY LASlib_INCLUDE_DIR LASlib_INCLUDE_DIR2)
 
 if(LASlib_FOUND)
 	message("—- Found LASlib under ${LASlib_INCLUDE_DIR}")
-    set(LASlib_INCLUDE_DIRS ${LASlib_INCLUDE_DIR})
+    set(LASlib_INCLUDE_DIRS ${LASlib_INCLUDE_DIR} ${LASlib_INCLUDE_DIR2})
     set(LASlib_LIBRARIES ${LASlib_LIBRARY})
     if(CMAKE_SYSTEM_NAME STREQUAL "Linux")
         set(LASlib_LIBRARIES "${LASlib_LIBRARIES};m;pthread")
