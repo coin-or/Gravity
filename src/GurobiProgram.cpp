6cf3e153cd7a679340250d94389f344fe3539c2c
Hassan <hlh@lanl.gov>
2021-03-02 21:13:11 -0700

    GRB_DoubleParam_NodefileStart

--- src/GurobiProgram.cpp
+++ src/GurobiProgram.cpp
@@ -208,9 +208,10 @@ bool GurobiProgram::solve(bool relax, double mipgap, bool use_callback){
 //    grb_mod->set(GRB_DoubleParam_MIPGap, 1e-8);
 //grb_mod->set(GRB_IntParam_Threads, 4);
 //    if(use_callback){
+        grb_mod->set(GRB_DoubleParam_NodefileStart,0.5);
         grb_mod->set(GRB_IntParam_NonConvex,2);
         grb_mod->set(GRB_IntParam_NumericFocus,3);
-        grb_mod->set(GRB_DoubleParam_TimeLimit,200);
+        grb_mod->set(GRB_DoubleParam_TimeLimit,9000);
         grb_mod->getEnv().set(GRB_IntParam_DualReductions, 0);
         grb_mod->getEnv().set(GRB_IntParam_PreCrush, 1);
         grb_mod->getEnv().set(GRB_IntParam_LazyConstraints, 1);
@@ -261,7 +262,7 @@ void GurobiProgram::prepare_model(){
     fill_in_grb_vmap();
     create_grb_constraints();
     set_grb_objective();
-    grb_mod->write("gurobiprint.lp");
+//    grb_mod->write("gurobiprint.lp");
         //    print_constraints();
 }
 void GurobiProgram::update_model(){
