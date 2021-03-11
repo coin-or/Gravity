f3e74b89d87e014d4186c02b36fb5dc56819fb4c
Hassan <hlh@lanl.gov>
2021-03-10 17:16:08 -0700

    getting ready to test GoICP

--- src/GurobiProgram.cpp
+++ src/GurobiProgram.cpp
@@ -275,10 +275,10 @@ bool GurobiProgram::solve(bool relax, double mipgap, bool use_callback){
         throw invalid_argument("Number of variables in Gurobi model does not match Gravity!");
     Model<> interior;
         //        _model->replace_integers();
-//    auto lin=_model->buildOA();
-//    interior=lin->add_outer_app_solution(*_model);
-//    cuts cb = cuts(_grb_vars, n, _model, interior);
-//    grb_mod->setCallback(&cb);
+    auto lin=_model->buildOA();
+    interior=lin->add_outer_app_solution(*_model);
+    cuts cb = cuts(_grb_vars, n, _model, interior);
+    grb_mod->setCallback(&cb);
         //    }
     grb_mod->optimize();
         //            grb_mod->write("~/mod.mps");
