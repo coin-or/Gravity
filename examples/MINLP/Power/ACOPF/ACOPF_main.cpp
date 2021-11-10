9ee1c6e541b4bd2de1796c653d6fb33cffe6df0d
Hassan Hijazi <hlh@lanl.gov>
2021-11-10 15:06:01 -0700

    fixing path to datasets

--- examples/MINLP/Power/ACOPF/ACOPF_main.cpp
+++ examples/MINLP/Power/ACOPF/ACOPF_main.cpp
@@ -21,7 +21,7 @@ using namespace gravity;
 
 int main (int argc, char * argv[])
 {
-    string fname = "../data_sets/Power/nesta_case5_pjm.m", mtype = "ACPOL";
+    string fname = string(prj_dir)+"/data_sets/Power/nesta_case5_pjm.m", mtype = "ACPOL";
     DebugOn("argv[0] =" << argv[0] << endl);
     string path = argv[0];
     int output = 0;
