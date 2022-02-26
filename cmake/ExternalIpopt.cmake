0b3553532bd8509d6b291e37ef7e418670ff44e1
Hassan <hlh@lanl.gov>
2022-02-26 14:36:27 -0700

    Update ExternalIpopt.cmake

--- cmake/ExternalIpopt.cmake
+++ cmake/ExternalIpopt.cmake
@@ -3,9 +3,9 @@ set(IPOPT_HOME https://www.coin-or.org/Tarballs/Ipopt)
 set(IPOPT_DOWNLOAD_URL ${IPOPT_HOME}/Ipopt-3.12.13.tgz)
 unset(IPOPT_HOME)
 
+set(IPOPT_ROOT_DIR ${PROJECT_SOURCE_DIR}/thirdparty/Ipopt CACHE INTERNAL "")
 
 if(WIN32)
-set(IPOPT_ROOT_DIR ${PROJECT_SOURCE_DIR}/thirdparty/Ipopt CACHE INTERNAL "")
 ExternalProject_Add(ipopt
     DOWNLOAD_DIR ${THIRDPARTY_INSTALL_PATH}
     DOWNLOAD_COMMAND curl -k -L https://github.com/IDAES/idaes-ext/releases/download/2.4.1/idaes-solvers-windows-64.tar.gz -o idaes-solvers-windows-64.tar.gz && tar -xvzf idaes-solvers-windows-64.tar.gz -C ${IPOPT_ROOT_DIR}
@@ -38,10 +38,9 @@ ${IPOPT_ROOT_DIR}/libgfortran-5.dll ${PROJECT_SOURCE_DIR}/bin/Release/libgfortra
 else()
 
 # Download and build the IPOPT library and add its properties to the third party arguments.
-set(IPOPT_ROOT_DIR ${THIRDPARTY_INSTALL_PATH}/Install/ipopt/build CACHE INTERNAL "")
 ExternalProject_Add(ipopt
     DOWNLOAD_DIR ${THIRDPARTY_INSTALL_PATH}
-    DOWNLOAD_COMMAND export HTTPS_PROXY=$ENV{HTTPS_PROXY} && curl -k -L ${IPOPT_DOWNLOAD_URL} -o Ipopt.tar.gz && tar -xzf Ipopt.tar.gz && mv Ipopt-3.12.13 ./Ipopt && cd ./Ipopt && mkdir build && cd ./ThirdParty/Mumps && export HTTP_PROXY=$ENV{HTTP_PROXY} && ./get.Mumps && cd ../../build && ../configure --prefix=${IPOPT_ROOT_DIR} && make install -j24
+    DOWNLOAD_COMMAND export HTTPS_PROXY=$ENV{HTTPS_PROXY} && curl -k -L ${IPOPT_DOWNLOAD_URL} -o Ipopt.tar.gz && tar -xzf Ipopt.tar.gz && mv Ipopt-3.12.13 ${IPOPT_ROOT_DIR} && cd ${IPOPT_ROOT_DIR} && mkdir build && cd ./ThirdParty/Mumps && export HTTP_PROXY=$ENV{HTTP_PROXY} && ./get.Mumps && cd ../../build && ../configure --prefix=${IPOPT_ROOT_DIR} && make install -j24
     URL ${IPOPT_DOWNLOAD_URL}
     CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${IPOPT_ROOT_DIR}
     CONFIGURE_COMMAND ""
@@ -50,21 +49,21 @@ ExternalProject_Add(ipopt
 )
 endif()
 list(APPEND GLOBAL_THIRDPARTY_LIB_ARGS "-DIPOPT_ROOT_DIR:PATH=${IPOPT_ROOT_DIR}")
-set(IPOPT_INCLUDE_DIRS ${THIRDPARTY_INSTALL_PATH}/Ipopt/build/include/coin)
+set(IPOPT_INCLUDE_DIRS ${IPOPT_ROOT_DIR}/include/coin)
 include_directories(${IPOPT_INCLUDE_DIRS})
 if(APPLE)
 find_library(IPOPT_LIBRARIES
         libipopt.dylib
         HINTS /usr/local/lib
-        HINTS ${THIRDPARTY_INSTALL_PATH}/Ipopt/build/lib
-        HINTS ${PROJECT_SOURCE_DIR}/third_party/CoinIpopt/build/lib
+        HINTS ${IPOPT_ROOT_DIR}/build/lib
         HINTS ${IPOPT_ROOT_DIR}/lib
+        HINTS ${PROJECT_SOURCE_DIR}/third_party/CoinIpopt/build/lib
 )
 elseif(UNIX)
 find_library(IPOPT_LIBRARIES
         libipopt.so
         HINTS /usr/local/lib
-        HINTS ${THIRDPARTY_INSTALL_PATH}/Ipopt/build/lib
+        HINTS ${IPOPT_ROOT_DIR}/build/lib
         HINTS ${IPOPT_ROOT_DIR}/lib
         HINTS ${PROJECT_SOURCE_DIR}/third_party/CoinIpopt/build/lib
 )
