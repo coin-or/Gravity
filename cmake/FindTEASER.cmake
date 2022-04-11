f9220cf15ba82f5885eae9bab97cfb5219ca7520
smitha <smitha@lanl.gov>
2022-04-11 14:21:23 -0600

    interface to teaser

---
+++ cmake/FindTEASER.cmake
@@ -0,0 +1,38 @@
+# Search supplied hint directories first if supplied.
+set(TEASER_ROOT_DIR "$ENV{TEASER_ROOT_DIR}" CACHE PATH "TEASER root directory.")
+if(TEASER_ROOT_DIR)
+ message("Looking for Teaser in ${TEASER_ROOT_DIR}")
+else(TEASER_ROOT_DIR)
+ message("TEASER_ROOT_DIR not provided.")
+endif(TEASER_ROOT_DIR)
+
+find_path(TEASER_INCLUDE_DIR
+  HINTS /usr/local/include
+  HINTS /usr/include
+  HINTS ${TEASER_ROOT_DIR/teaser/include/teaser)
+
+# Set standard CMake FindPackage variables if found.
+if (TEASER_FOUND)
+  set(TEASER_INCLUDE_DIRS ${Teaser_INCLUDE_DIR})
+else (TEASER_FOUND)
+ message("Cannot find Teaser")
+endif (TEASER_FOUND)
+
+find_library(TEASER_LIBRARY 
+	libteaser_registration.so
+	HINTS ${TEASER_ROOT_DIR}/build/teaser
+)
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(TEASER DEFAULT_MSG TEASER_LIBRARY)
+
+if(TEASER_FOUND)
+	message("—- Found TEASER under ${TEASER_INCLUDE_DIR}")
+    set(TEASER_INCLUDE_DIRS ${TEASER_INCLUDE_DIR})
+    set(TEASER_LIBRARIES ${TEASER_LIBRARY})
+    if(CMAKE_SYSTEM_NAME STREQUAL "Linux")
+        set(TEASER_LIBRARIES "${TEASER_LIBRARIES};m;pthread")
+    endif(CMAKE_SYSTEM_NAME STREQUAL "Linux")
+endif(TEASER_FOUND)
+
+mark_as_advanced(TEASER_LIBRARY TEASER_INCLUDE_DIR)
