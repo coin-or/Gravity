3e5ba363355e8a4795a646dcee3c409d96a53821
smitha <smitha@lanl.gov>
2022-07-12 16:32:59 -0600

    testing projdev for misdp

---
+++ examples/Optimization/MINLP/MISDP/read_misdp.h
@@ -0,0 +1,17 @@
+//
+//  read_misdp.hpp
+//  misdp
+//
+//  Created by Smitha on 7/7/22.
+//
+#ifndef read_misdp_h
+#define read_misdp_h
+#include <gravity/solver.h>
+#include <stdio.h>
+
+using namespace gravity;
+using namespace std;
+
+void CBF_read(const char *file, shared_ptr<Model<double>>& m);
+
+#endif /* read_misdp_hpp */
