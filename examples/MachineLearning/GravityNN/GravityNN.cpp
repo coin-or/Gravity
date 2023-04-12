d69fdbf6403c4b04c53f177b94c9d3f419ebc2ba
Hassan <hlh@lanl.gov>
2023-04-12 12:57:34 -0600

    working on constraint indexing

--- examples/MachineLearning/GravityNN/GravityNN.cpp
+++ examples/MachineLearning/GravityNN/GravityNN.cpp
@@ -55,7 +55,11 @@ int main() {
     
     auto layers = build_graph(graph);
     auto input_dims = get_input_dim(graph.input());/* Getting input layer dim */
+    /* INDEX SETS */
+    /* Indexing variables */
     indices x_ids("x_ids"), y_ids("y_ids");/*< x_ids for continuous vars, y_ids for binary vars */
+
+    /* Creating variables' indexing sets */
     for(auto j = 0; j < input_dims[1];j++){
         x_ids.add("input"+to_string(j));
     }
@@ -64,9 +68,9 @@ int main() {
     bool is_activation_func = false;
     for(auto i = 0; i<nb_layers; i++){
         auto l = layers[i];
-        if(i+1<nb_layers && layers[i+1]->is_activation_func)/*< The next layer is an activation layer (e.g. ReLU), no need to introduce variables for this layer */
+        if(i+1<nb_layers && layers[i+1]->is_activation_func){/*< The next layer is an activation layer (e.g. ReLU), no need to introduce variables for this layer */
             continue;
-        indices M("M_"+l->name);
+        }
         if(l->is_activation_func){/*< Activation function such as ReLU, use the output of the prvious layer to index the RelU output vars */
             dims = layers[i-1]->var_dims;
         }
@@ -80,16 +84,44 @@ int main() {
             }
         }
     }
-    
+    /* Indexing constraints */
+    indices ReLUs("ReLUs"), x_ReLUs("x_ReLUs");
+    x_ReLUs = x_ids;
+    size_t idx = 0;
+    /* Creating RelU indexing sets */
+    for(auto i = 0; i<nb_layers; i++){
+        auto l = layers[i];
+        if(l->is_activation_func){/*< Activation function such as ReLU, use the output of the prvious layer to index the RelU output vars */
+            for(auto j = 0; j < layers[i-1]->var_dims[1];j++){
+                ReLUs.add(l->name+","+to_string(j));
+            }
+        }
+        else if(i+1<nb_layers && layers[i+1]->is_activation_func){/*< The next layer is an activation layer (e.g. ReLU), no need to introduce variables for this layer */
+            for(auto j = 0; j < l->var_dims[1];j++){
+                for(auto k = 0; k < l->var_dims[0];k++){
+                    x_ReLUs.add_in_row(idx, l->name+","+to_string(k));
+                }
+                idx++;
+            }
+            continue;
+        }
+        
+    }
     
     Model<> NN("NN_"+fname.substr(fname.find_last_of("/")));
-    var<> x("x");
+    var<> x("x", -1, 1);
     var<int> y("y", 0, 1);
     NN.add(x.in(x_ids));
     NN.add(y.in(y_ids));
+    Constraint<> ReLU("ReLU");
+    ReLU = x.in(x_ReLUs) - x.in(ReLUs);// make this one disjunctive in y
+    NN.add(ReLU.in(ReLUs) == 0);
+
     NN.print();
     for(auto l:layers){
         delete l;
     }
+    
+
     return 0;
 }
